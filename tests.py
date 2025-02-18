dadc35e525ca0ef52efdf17892d585b2bf793685
ScottGreenhalgh <scottgreenhalgh62@me.com>
2025-02-18 08:00:01 +0000

    testing a fresh dockerfile

---
+++ tests.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Define options for running the chromedriver
+chrome_options = Options()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+# Initialize a new chrome driver instance
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get('https://www.example.com/')
+header_text = driver.find_element_by_xpath('//h1').text
+
+print("example.com Header text is:")
+print(header_text)
+
+# driver.get("http://localhost:3000")
+# header_text = driver.find_element_by_xpath('//h1').text
+
+# print("Our Homepage Header text is:")
+# print(header_text)
+
+driver.quit()
